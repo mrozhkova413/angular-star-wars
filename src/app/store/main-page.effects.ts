627a6fb92f9aca0f39ab7b535e8916defdec7fab
Maria <maria.rozhkova@bravedevelopers.com>
2022-01-06 21:00:18 +0000

    commit

--- src/app/store/main-page.effects.ts
+++ src/app/store/main-page.effects.ts
@@ -1,7 +1,7 @@
 import { loadList, loadListSuccess, loadListError } from './main-page.actions';
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, catchError, concatMap } from 'rxjs/operators';
+import { map, catchError, switchMap } from 'rxjs/operators';
 import { SwapiService } from '../swapi/swapi.service';
 import { of, combineLatest } from 'rxjs';
 import { ALL } from '../swapi/swapi.models';
@@ -12,7 +12,7 @@ export class MainPageEffects {
   loadListCharacters$ = createEffect(() => this.actions$
   .pipe(
     ofType(loadList),
-    concatMap((action) => combineLatest(
+    switchMap((action) => combineLatest(
       action.sections.map(section => this.apiService.getList(section, action.search)))
   )).pipe(
         map(list => loadListSuccess( { list: list.reduce((accumulator, value) => accumulator.concat(value.results), [] as ALL[]) })),
