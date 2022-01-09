07ae5234086e66b5eb8585f728236c00631a22d1
Maria <maria.rozhkova@bravedevelopers.com>
2022-01-09 17:38:12 +0000

    commit

--- src/app/store/main-page.reducer.ts
+++ src/app/store/main-page.reducer.ts
@@ -23,24 +23,24 @@ function filterList(list: ALL[], filters: Filters): ALL[] {
   if (filters.people) {
     let fil = filters.people;
     return list.map(x => x as People).filter(
-      x => (fil.hairColor ? x.hair_color === fil.hairColor : true) && 
-      (fil.eyesColor ? x.eye_color === fil.eyesColor : true) && 
-      (fil.gender ? x.gender === fil.gender : true));  
+      x => (fil.hairColor ? x.hair_color === fil.hairColor : true) &&
+      (fil.eyesColor ? x.eye_color === fil.eyesColor : true) &&
+      (fil.gender ? x.gender === fil.gender : true));
   }
 
   if (filters.planets) {
     let fil = filters.planets;
     return list.map(x => x as Planet).filter(
-      x => (fil.terrain ? x.terrain.includes(fil.terrain) : true) && 
-      (fil.climate ? x.climate === fil.climate : true) && 
-      (fil.rotation_period ? x.rotation_period >= fil.rotation_period : true)); 
+      x => (fil.terrain ? x.terrain.includes(fil.terrain) : true) &&
+      (fil.climate ? x.climate === fil.climate : true) &&
+      (fil.rotation_period ? x.rotation_period >= fil.rotation_period : true));
   }
 
   if (filters.starships) {
     let fil = filters.starships;
     return list.map(x => x as Starship).filter(
-      x => ((fil.max_atmosphering_speed || x.max_atmosphering_speed !== 'n/a') ? Number(x.max_atmosphering_speed) >= fil.max_atmosphering_speed : true) && 
-      ((fil.passengers || x.passengers !== 'n/a') ? Number(x.passengers) >= fil.passengers : true)); 
+      x => ((fil.max_atmosphering_speed || x.max_atmosphering_speed !== 'n/a') ? Number(x.max_atmosphering_speed) >= fil.max_atmosphering_speed : true) &&
+      ((fil.passengers || x.passengers !== 'n/a') ? Number(x.passengers) >= fil.passengers : true));
   }
 
   return list;
