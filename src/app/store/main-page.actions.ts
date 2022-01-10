e53617eac201fd135cf965005c7bed20c04e27a4
Maria <maria.rozhkova@bravedevelopers.com>
2022-01-10 01:46:10 +0000

    commit

--- src/app/store/main-page.actions.ts
+++ src/app/store/main-page.actions.ts
@@ -3,7 +3,6 @@ import { ALL } from '../swapi/swapi.models';
 import { Filters, Sections } from '../swapi/filter.models';
 
 export const select = createAction('[Main Page] Select', props<{ id: string; }>());
-export const searchInput = createAction('[Main Page] Search input', props<{ search: string; }>());
 export const loadList = createAction('[Main Page] Load list', props<{ sections: Sections[]; search: string }>());
 export const loadListSuccess = createAction('[Main Page] Load list success', props<{ list: ALL[]; }>());
 export const filterList = createAction('[Main Page] Filter list', props<{ filters: Filters; }>());
