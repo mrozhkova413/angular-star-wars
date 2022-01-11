7cf2386c84a96f2a3321390172616d343fea8614
Maria <maria.rozhkova@bravedevelopers.com>
2022-01-11 00:39:13 +0000

    commit

--- src/app/common/hover-bold.directive.spec.ts
+++ src/app/common/hover-bold.directive.spec.ts
@@ -1,22 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HoverBoldDirective } from './hover-bold.directive';
-import { ElementRef } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-export class MockElementRef extends ElementRef {}
-let elRef: ElementRef;
+@Component({
+  template: `
+  <h2 appHoverBold data-testid="header">About</h2>
+  `
+})
+export class AboutComponent { }
 
 describe('HoverBoldDirective', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [
-        // { provide: ElementRef, useClass: MockElementRef }
-      ]
-    }).compileComponents();
-    elRef = TestBed.get(ElementRef);
+  let fixture: ComponentFixture<AboutComponent>;
+  let header: HTMLHeadingElement;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [ AboutComponent, HoverBoldDirective ],
+      schemas:      [ CUSTOM_ELEMENTS_SCHEMA ]
+    })
+    .createComponent(AboutComponent);
+    fixture.detectChanges();
+
+    header = fixture.debugElement.query(By.css(`[data-testid="header"]`)).nativeElement;
   });
 
-  it('should create an instance', () => {
-    const directive = new HoverBoldDirective(elRef);
-    expect(directive).toBeTruthy();
+  it('should be bold on hover', () => {
+    expect(header.style.fontWeight === '').toBeTrue();
+
+    header.dispatchEvent(new MouseEvent('mouseenter', {
+          view: window,
+          bubbles: true,
+          cancelable: true
+        }));
+    expect(header.style.fontWeight === '570').toBeTrue();
+
+    header.dispatchEvent(new MouseEvent('mouseleave', {
+      view: window,
+      bubbles: true,
+      cancelable: true
+    }));
+    expect(header.style.fontWeight === '').toBeTrue();
   });
 });
